c316339e969e0e1ea874db96dcac599100a75047
gajigaji0401 <gajigaji04@naver.com>
2023-07-06 19:04:27 +0900

    sequelize transaction

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { Users, UserInfos } = require("../models");
+const { Users, UserInfos, UserHistories, sequelize } = require("../models");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const { Transaction } = require("sequelize");
+const authMiddleware = require("../middlewares/auth-middleware");
 
 // 회원가입
 router.post("/users", async (req, res) => {
@@ -12,39 +14,47 @@ router.post("/users", async (req, res) => {
     return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
   }
 
-  // Users 테이블에 사용자를 추가합니다.
-  const user = await Users.create({ email, password });
-  // UserInfos 테이블에 사용자 정보를 추가합니다.
-  const userInfo = await UserInfos.create({
-    UserId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
-    name,
-    age,
-    gender: gender.toUpperCase(), // 성별을 대문자로 변환합니다.
-    profileImage,
+  const t = await sequelize.transaction({
+    isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED,
   });
+
+  try {
+    const user = await Users.create({ email, password }, { transaction: t });
+
+    const userInfo = await UserInfos.create(
+      {
+        UserId: user.userId,
+        name,
+        age,
+        gender: gender.toUpperCase(),
+        profileImage,
+      },
+      { transaction: t }
+    );
+
+    await t.commit();
+  } catch (transactionError) {
+    console.error(transactionError);
+    await t.rollback();
+    return res
+      .status(400)
+      .json({ errorMessage: "유저 생성에 실패하였습니다." });
+  }
 
   return res.status(201).json({ message: "회원가입이 완료되었습니다." });
 });
-
-// routes/users.route.js
 
 // 로그인
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await Users.findOne({ where: { email } });
 
-  // 1. 해당하는 사용자가 존재?
-  // 2. 해당하는 사용자의 비밀번호 존재?
   if (!user) {
     return res.status(401).json({ message: "존재하지 않는 이메일입니다." });
   } else if (user.password !== password) {
     return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
   }
 
-  // jwt 생성
-
-  // 쿠키를 발급
-  // response 할당
   const token = jwt.sign(
     {
       userId: user.userId,
@@ -55,8 +65,6 @@ router.post("/login", async (req, res) => {
   return res.status(200).json({ message: "로그인 성공" });
 });
 
-module.exports = router;
-
 // 사용자 조회
 router.get("/users/:userId", async (req, res) => {
   const { userId } = req.params;
@@ -65,7 +73,7 @@ router.get("/users/:userId", async (req, res) => {
     attributes: ["userId", "email", "createdAt", "updatedAt"],
     include: [
       {
-        model: UserInfos, // 1:1 관계를 맺고있는 UserInfos 테이블을 조회합니다.
+        model: UserInfos,
         attributes: ["name", "age", "gender", "profileImage"],
       },
     ],
@@ -74,3 +82,49 @@ router.get("/users/:userId", async (req, res) => {
 
   return res.status(200).json({ data: user });
 });
+
+// routes/users.route.js
+
+// 사용자 이름 변경 API
+router.put("/users/name", authMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+  const { name } = req.body;
+
+  const userInfo = await UserInfos.findOne({ where: { userId } });
+  const beforeUserName = userInfo.name;
+
+  // MySQL과 연결된 sequelize connection에서 transaction을 생성합니다.
+  const t = await sequelize.transaction({
+    isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED, // 트랜잭션 격리 수준을 설정합니다.
+  });
+  try {
+    await UserInfos.update(
+      { name },
+      {
+        where: { userId },
+        transaction: t, // transaction을 사용합니다.
+      }
+    );
+
+    await UserHistories.create(
+      {
+        UserId: userId,
+        beforeName: beforeUserName,
+        afterName: name,
+      },
+      { transaction: t } // transaction을 사용합니다.
+    );
+
+    t.commit();
+  } catch (transactionError) {
+    console.error(transactionError);
+    t.rollback();
+    return res
+      .status(400)
+      .json({ errorMessage: `유저 이름 변경에 실패하였습니다.` });
+  }
+
+  return res.status(200).json({ message: "유저 이름 변경에 성공하였습니다." });
+});
+
+module.exports = router;
